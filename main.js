5221026b5fd81ef7112ab910c3be59e7cd310f2c
moisesggk <138232254+moisesggk@users.noreply.github.com>
2025-06-06 09:21:26 -0300

    nao finalizado

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ for (let i =0; i< botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll9=(".contador");
-const tempoObjetivo = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-10-05T00:00:00");
 
 
 contadores[0].textContent =calculaTempo(tempoObjetivo);
@@ -33,5 +35,5 @@ function calculaTempo(tempoObjetivo){
     horas %=24;
 
     return dias + "dias" + "horas" +  "horas " + minutos" + "mitutos" + "segundos" + "segundos"; 
-    
+
 }
